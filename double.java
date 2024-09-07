d853a4e4956c3ecf67755868fbbfe7935bdff83a
vamsid23 <vamsidanduboina23@gmail.comgit config --global -egit config --global user.email vamsidanduboina23@gmail.com>
2024-09-07 23:13:57 +0530

    double file

---
+++ double.java
@@ -0,0 +1,13 @@
+class Double
+{
+    public static void main(String arg[])
+    {
+     int a=195;
+     int fd=195%10;
+     if(fd%4==0)
+     
+     System.out.println(fd*fd+ "number is double");
+     else
+      System.out.println("not divisible");
+     }
+}
